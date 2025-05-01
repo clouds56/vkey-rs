bda327602ef8ec493977f6f2dbf4e0e4db6adf3e
Clouds Flowing <clouds.flowing@gmail.com>
2025-05-01 13:35:12 +0800

    fix features

--- src/deps/mod.rs
+++ src/deps/mod.rs
@@ -2,7 +2,11 @@
 pub use ::hut_03;
 #[cfg(dep_hut_04)]
 pub use ::hut_04;
+
+#[cfg(dep_hut_04)]
 pub use hut_04 as hut;
+#[cfg(all(not(dep_hut_04), dep_hut_03))]
+pub use hut_03 as hut;
 
 #[cfg(dep_windows_vk)]
 pub use ::windows::Win32::UI::Input::KeyboardAndMouse::{self as windows, VIRTUAL_KEY};
